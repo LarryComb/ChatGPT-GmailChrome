e034db45cf2fa6826259a99f0574d1b86d790863
larry <larrycomb@gmail.com>
2023-02-12 09:23:24 -0800

    reseting code

--- ChatGPT_Gmail/background.js
+++ ChatGPT_Gmail/background.js
@@ -3,11 +3,11 @@ console.log('hello i am the service worker');
 chrome.runtime.onMessage.addListener(
     function(emailContent, sender, senderResponse) {
         console.log(emailContent);
-        (async function() {
+        /*(async function() {
             const tabs = await chrome.tabs.query({url: 'https://chat.openai.com/*'});
             const tab = tabs[0];
             chrome.tabs.sendMessage(tab.id, emailContent);
-        })();
+        })();*/
     }
     
 
